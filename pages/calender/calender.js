ae5f73046a232b7fa16af4b98027071e70060989
PKUer_ztw <625581835@qq.com>
2019-01-19 17:50:46 +0800

    Initial Commit

---
+++ pages/calender/calender.js
@@ -0,0 +1,112 @@
+// pages/calender/calender.js
+var app = getApp();
+Page({
+  data: {
+    currentDate: "2019年01月01日",
+    dayList: '',
+    currentDayList: '',
+    currentObj: '',
+    currentDay: '',
+    routine:'',
+    number:0,
+    str1:''
+  },
+  onLoad: function (options) {
+    var currentObj = this.getCurrentDayString()
+    var number = 0
+    var i,sum=0
+    var now = 10
+    var str1="课程周"
+    //console.log(currentObj.getMonth()+1)
+    for (i=9;i<currentObj.getMonth()+13;i++)
+    {
+      var days
+      if(i==10||i==12||i==13||i==15) days=31
+      else days=30
+      sum=sum+(days-now)+1
+      now=1
+    }
+    //console.log(sum)
+    sum = sum+currentObj.getDate()-now+1
+    number = Math.ceil(sum / 7)
+    if (number < 20 && number > 17) str1 = "考试周"
+    else str1 = "课程周"
+    this.setData({
+      currentDate: currentObj.getFullYear() + '年' + (currentObj.getMonth() + 1) + '月' + currentObj.getDate() + '日',
+      currentDay: currentObj.getDate(),
+      currentObj: currentObj,
+      number:number,
+      str1:str1,
+      routine: '第'+ number +'周: '+str1
+    })
+    this.setSchedule(currentObj)
+  },
+  doDay: function (e) {
+    var that = this
+    var currentObj = that.data.currentObj
+    var Y = currentObj.getFullYear();
+    var m = currentObj.getMonth() + 1;
+    var d = currentObj.getDate();
+    var str = ''
+    if (e.currentTarget.dataset.key == 'left') {
+      m -= 1
+      if (m <= 0) {
+        str = (Y - 1) + '/' + 12 + '/' + d
+      } else {
+        str = Y + '/' + m + '/' + d
+      }
+    } else {
+      m += 1
+      if (m <= 12) {
+        str = Y + '/' + m + '/' + d
+      } else {
+        str = (Y + 1) + '/' + 1 + '/' + d
+      }
+    }
+    currentObj = new Date(str)
+    this.setData({
+      currentDate: currentObj.getFullYear() + '年' + (currentObj.getMonth() + 1) + '月' + currentObj.getDate() + '日',
+      currentObj: currentObj
+    })
+    this.setSchedule(currentObj);
+  },
+  getCurrentDayString: function () {
+    var objDate = this.data.currentObj
+    if (objDate != '') {
+      return objDate
+    } else {
+      var c_obj = new Date()
+      var a = c_obj.getFullYear() + '/' + (c_obj.getMonth() + 1) + '/' + c_obj.getDate()
+      return new Date(a)
+    }
+  },
+  setSchedule: function (currentObj) {
+    var that = this
+    var m = currentObj.getMonth() + 1
+    var Y = currentObj.getFullYear()
+    var d = currentObj.getDate();
+    var dayString = Y + '/' + m + '/' + currentObj.getDate()
+    var currentDayNum = new Date(Y, m, 0).getDate()
+    var currentDayWeek = currentObj.getUTCDay() + 1
+    var result = currentDayWeek - (d % 7 - 1);
+    var firstKey = result <= 0 ? 7 + result : result;
+    var currentDayList = []
+    var f = 0
+    for (var i = 0; i < 42; i++) {
+      let data = []
+      if (i < firstKey - 1) {
+        currentDayList[i] = ''
+      } else {
+        if (f < currentDayNum) {
+          currentDayList[i] = f + 1
+          f = currentDayList[i]
+        } else if (f >= currentDayNum) {
+          currentDayList[i] = ''
+        }
+      }
+    }
+    that.setData({
+      currentDayList: currentDayList
+    })
+  }
+})
